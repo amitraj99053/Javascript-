6c2f0f49005bdecb6eb82423629ad2e62d8a0db6
Amit Kumar <amitraj99053@gmail.com>
2026-02-08 00:00:51 +0530

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,7 +32,7 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+//  console.log(negValue);
 
 // console.log(2+2);
 // console.log(2-2);
@@ -60,6 +60,8 @@ let str3 = str1 + str2
 let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
+
+// console.table([num1, num2, num3]);
 
 let gameCounter = 100
 ++gameCounter;
