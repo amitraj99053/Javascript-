a0fd0ca24b6824226e2b1153e37572fdc21e7147
Amit Kumar <amitraj99053@gmail.com>
2026-02-12 02:22:37 +0530

    Arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,54 @@
+const user = {
+    username: "Amit",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai() {
+//     let username = "Amit";
+//     console.log(this.username);
+// }
+
+// chai()
+
+
+// const chai = function () {
+//     let username = "Amit";
+//     console.log(this.username);
+// }
+
+const chai = () => { // Arrow function
+    let username = "Amit";
+    console.log(this);
+}
+
+// chai();
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// explicit return
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => (num1 + num2);
+
+// const addTwo = (num1, num2) => {username: "Amit"};
+const addTwo = (num1, num2) => ({username: "Amit"});
+
+console.log(addTwo(5, 4));
+
+// const myArray = [2, 5, 8, 9, 7];
+
+// myArray.forEach();
